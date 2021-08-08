8c9699450b52a5c2e87480bfdcdf1a03a6024aba
tropik555 <yevhen.levchenko@nure.ua>
2021-08-08 13:32:04 +0300

    new Branch

--- src/main/java/fundamentals/hackerRank/OOP/JavaAbstractClass/Solution.java
+++ src/main/java/fundamentals/hackerRank/OOP/JavaAbstractClass/Solution.java
@@ -26,7 +26,7 @@ public class Solution {
         String title = sc.nextLine();
         MyBook new_novel = new MyBook();
         new_novel.setTitle(title);
-        System.out.println("The title is : " + new_novel.getTitle());
+        System.out.println("The title is: " + new_novel.getTitle());
         sc.close();
     }
 }
