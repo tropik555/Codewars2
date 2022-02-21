655283e924ead83d244697f5964e58a7d82ae8ec
tropik555 <yevhen.levchenko@nure.ua>
2022-02-21 11:21:14 +0200

    u1v2

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -3,7 +3,5 @@ package git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("L-01");
-        System.out.println("L-03");
-        System.out.println("L-02");
     }
 }
