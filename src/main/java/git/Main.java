49612e4b8fecea0b7afa9a06174469d35151fef5
tropik555 <yevhen.levchenko@nure.ua>
2022-02-21 11:31:32 +0200

    u1v2

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("L-01");
         System.out.println("L-02");
+        System.out.println("L-02");
+        System.out.println("L-02");
+
     }
 }
