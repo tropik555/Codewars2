ed562caa53a4422d3441c527effd6354441dbdbe
tropik555 <67749609+tropik555@users.noreply.github.com>
2022-02-21 11:18:47 +0200

    o2 (#10)
    * o2
    
    * o3
    
    * o3

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("L-01");
         System.out.println("L-03");
+        System.out.println("L-02");
     }
 }
